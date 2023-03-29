9c1ace456d916f5782b7ef4b4b1e9e2452a055e2
vnstartupvan <vn.startup99@gmail.com>
2023-03-29 20:57:54 +0700

    temp fix: origin url

--- Components/Toolbar/Toolbar.feature.ts
+++ Components/Toolbar/Toolbar.feature.ts
@@ -1,9 +1,10 @@
 import { RoomInfoPayload } from './../../libs/models/room';
 import { fetchRoomURL } from '@libs/api/room';
+import env from '@constants/env';
 
 export const handleCreateRoom = async (room: RoomInfoPayload) => {
     const newRoom = await fetchRoomURL(room);
     const roomURL = newRoom.url;
-    window.open(`${window.location.origin}/room/${roomURL}`);
+    window.open(`${env.appUrl}/room/${roomURL}`);
     return newRoom;
 };
--- constants/env.ts
+++ constants/env.ts
@@ -1,6 +1,7 @@
 const env = {
     // apiUrl: process.env.NEXT_PUBLIC_API_URL,
     apiUrl: 'https://engtalkfordevserver.onrender.com',
+    appUrl: 'https://engtalkfordev-client-git-main-vnstartupvan.vercel.app',
     // apiUrl: 'http://localhost:3001',
 };
 
